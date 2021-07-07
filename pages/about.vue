971e64ceb3711763d9de97b3d2eec3c0160a91f4
Jason Rivera <jp_rivera_@hotmail.com>
2021-07-07 10:57:19 -0700

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>This is the about page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "About",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "About for best place for corny dad jokes"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped></style>
